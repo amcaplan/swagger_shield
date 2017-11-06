ce785c7e9e0ef9657fae621ba61ac01441822e7f
amcaplan <ariel.caplan@mail.yu.edu>
2017-11-06 19:19:02 +0200

    Allow SwaggerShield to work via a simple incantation
    In the controller, write:
    
    SwaggerShield.protect!(self, swagger_file: filename)
    
    and you're done!

--- lib/swagger_shield.rb
+++ lib/swagger_shield.rb
@@ -3,5 +3,30 @@ require "json-schema"
 require "swagger_shield/shield"
 
 module SwaggerShield
-  # Your code goes here...
+  class << self
+    def protect!(controller, swagger_file:)
+      swagger_shield = SwaggerShield::Shield.new(
+        YAML.load_file(swagger_file)
+      )
+
+      controller.before_action do
+        errors = swagger_shield.validate(
+          request.path,
+          request.method,
+          params.to_unsafe_h
+        )
+
+        unless errors.empty?
+          formatted = errors.map { |error|
+            {
+              status: '422',
+              detail: error[:message],
+              source: { pointer: error[:fragment] }
+            }
+          }
+          render json: { errors: formatted }, status: :unprocessable_entity
+        end
+      end
+    end
+  end
 end
--- spec/fixtures/rails_app/app/controllers/application_controller.rb
+++ spec/fixtures/rails_app/app/controllers/application_controller.rb
@@ -1,28 +1,6 @@
 class ApplicationController < ActionController::API
-  before_action :shield!
-
-  def shield!
-    errors = swagger_shield.validate(
-      request.path,
-      request.method,
-      params.to_unsafe_h
-    )
-
-    unless errors.empty?
-      formatted = errors.map { |error|
-        {
-          status: '422',
-          detail: error[:message],
-          source: { pointer: error[:fragment] }
-        }
-      }
-      render json: { errors: formatted }, status: :unprocessable_entity
-    end
-  end
-
-  def swagger_shield
-    @swagger_shield ||= SwaggerShield::Shield.new(
-      YAML.load_file(File.join 'spec', 'fixtures', 'swagger.yml')
-    )
-  end
+  SwaggerShield.protect!(
+    self,
+    swagger_file: File.join('spec', 'fixtures', 'swagger.yml')
+  )
 end
